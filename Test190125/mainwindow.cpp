057c914ace45fdfdab38e353db0e4b1551609c1e
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-25 20:25:45 +0900

    Add files via upload

---
+++ Test190125/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->horizontalSlider, SIGNAL(valueChanged(int)),ui->progressBar, SLOT(setValue(int)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->setText("Go to home!");
+}
