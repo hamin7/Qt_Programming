d676b9a58c7b093c98c65d4d7560c2dcda96a508
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-26 21:12:17 +0900

    Add files via upload

--- Test190126/mainwindow.cpp
+++ Test190126/mainwindow.cpp
@@ -2,6 +2,9 @@
 #include "ui_mainwindow.h"
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QDesktopServices>
+#include <QUrl>
+#include <QProcess>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,11 +20,20 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QString filename = QFileDialog::getOpenFileName(
-                this,
-                tr("Open File"),            // 열리는 탐색기 상단의 이름.
-                "C://",                 // C디스크가 열림.
-                "All files (*.*);;Text File (*.txt);;Music file(*.mp3)"           // * star means any file you want to open.  // (*.txt)means all txt files. // 오른쪽 하단에 세가지 고르는 상자나옴.
-                );
-    QMessageBox::information(this,tr("File Name"), filename);
+    //QString filename = QFileDialog::getOpenFileName(            // filename변수는 file의 path임.
+    //            this,
+    //            tr("Open File"),            // 열리는 탐색기 상단의 이름.
+    //            "C:\\Avatar\\",                 // C디스크가 열림.
+    //            "All files (*.*);;Text File (*.txt);;Music file(*.mp3)"           // * star means any file you want to open.  // (*.txt)means all txt files. // 오른쪽 하단에 세가지 고르는 상자나옴.
+    //            );
+
+    //QDesktopServices::openUrl(QUrl("file:///"+filename, QUrl::TolerantMode));            // TolerantMode-windows file system.
+    //QMessageBox::information(this,tr("File Name"), filename);
+
+    system("start C:\\Avatar\\Server.exe");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    system("start C:\\Avatar\\realTimeClassificationToMaya\\application\\realTimeClassificationToMaya.exe");
 }
